288be33307a69bd167ce7dd1377f7ae6629fbfda
whereishikaru <h.suzuki.028@gmail.com>
2024-10-04 20:14:27 +1300

    refactor: add project data

--- src/components/projects/ProjectData.tsx
+++ src/components/projects/ProjectData.tsx
@@ -1,7 +1,15 @@
 export const ProjectData: Project[] = [
     {
+        image: '/png/mediscan.png',
+        category: ['Python', 'Next.js', 'React Native', 'TypeScript', 'Tailwind CSS', 'HTML', 'CSS'],
+        title: 'Mediscan',
+        description: 'This is an image recognition application for Healthcare and a user-friendly AI-driven solution designed to transform how you access pharmacy products and services.',
+        link: '',
+        github: 'https://github.com/hikaru028/mediscan',
+    },
+    {
         image: '/png/portfolio.png',
-        category: 'Next.js',
+        category: ['Next.js', 'TypeScript', 'Tailwind CSS'],
         title: 'Hikari. (My Portfolio)',
         description: 'This is my portfolio website, which showcases my skills. It embodies a modern design, a visually appealing and intuitive user experience.',
         link: 'https://hikarusuzuki.link/portfolio',
@@ -9,15 +17,15 @@ export const ProjectData: Project[] = [
     },
     {
         image: '/png/x-clone.png',
-        category: 'Node.js',
+        category: ['MongoDB', 'Express', 'React', 'Node.js', 'TypeScript', 'Tailwind CSS'],
         title: 'X-clone app',
         description: 'A full-stack Twitter clone application built using Express, MongoDB, and Vite with React.',
-        link: '-',
+        link: '',
         github: 'https://github.com/hikaru028/Twitter-Clone',
     },
     {
         image: '/png/kalah.png',
-        category: 'Java',
+        category: ['Java'],
         title: 'Kalah Game',
         description: 'This was developed as part of my coursework. This project focused on studying maintainable software design principles.',
         link: '',
@@ -25,7 +33,7 @@ export const ProjectData: Project[] = [
     },
     {
         image: '/png/noimage.png',
-        category: 'C#',
+        category: ['C#', '.NET', 'HTML', 'CSS'],
         title: 'Othello Game',
         description: 'The server pairs up players and coordinates the exchange of game moves. It is built using the C# programming language and .NET 7.',
         link: '',
@@ -35,7 +43,7 @@ export const ProjectData: Project[] = [
 
 interface Project {
     image?: string;
-    category?: string;
+    category?: string[];
     title?: string;
     tool?: string;
     description?: string;
